691f87f58c16ac905c3a476ba10c6c49519aad30
root <root@nas.littlefish12345.com>
2021-05-17 00:01:53 +0800

    aes encrypt function with bugs

--- PIMS-base.h
+++ PIMS-base.h
@@ -3,14 +3,31 @@
 #define PIMS_BASE_H
 
 #include<openssl/sha.h>
+#include<openssl/md5.h>
 #include<openssl/aes.h>
 #include<openssl/rsa.h>
-#include<string.h>
 #include<stdlib.h>
+#include<string.h>
 
-unsigned char* doubleSHA256(const char* input) {
+typedef struct {
+    unsigned int length;
+    unsigned char* encrypted_data;
+}encryptedData;
+
+unsigned char* doubleMD5(const unsigned char* input, unsigned int length) {
+    unsigned char* result1 = (unsigned char*)malloc(16);
+    MD5((const unsigned char*)input,length,result1);
+    
+    unsigned char* result2 = (unsigned char*)malloc(16);
+    MD5((const unsigned char*)result1,16,result2);
+    
+    free(result1);
+    return result2;
+}
+
+unsigned char* doubleSHA256(const unsigned char* input, unsigned int length) {
     unsigned char* result1 = (unsigned char*)malloc(32);
-    SHA256((const unsigned char*)input,strlen(input),result1);
+    SHA256((const unsigned char*)input,length,result1);
     
     unsigned char* result2 = (unsigned char*)malloc(32);
     SHA256((const unsigned char*)result1,32,result2);
@@ -19,34 +36,65 @@ unsigned char* doubleSHA256(const char* input) {
     return result2;
 }
 
-unsigned char* SHA256EncryptAES128CBC(const unsigned char* plain_data,unsigned char* hash) {
-    unsigned char iv[16];
-    unsigned char* key = hash + 16;
-    memcpy((void*)(&iv),(void*)hash,16);
+encryptedData* AES256CBCEncrypt(const unsigned char* plain_data, unsigned int plain_data_length, unsigned char* key, unsigned char* iv) {
     AES_KEY aes_key;
-    AES_set_encrypt_key((const unsigned char*)key,128,&aes_key);
+    AES_set_encrypt_key((const unsigned char*)key,256,&aes_key);
     
-    unsigned int plain_length = strlen((const char*)plain_data);
-    unsigned char padding = 16 - plain_length % 16;
-    unsigned int all_plain_data_length = plain_length + padding;
-    unsigned char* all_plain_data = (unsigned char*)malloc(all_plain_data_length);
+    unsigned char padding = 16 - plain_data_length % 16;
+    unsigned int all_plain_data_length = plain_data_length + padding;
+    unsigned char* all_plain_data = (unsigned char*)malloc(all_plain_data_length+16);
     
-    memcpy((void*)all_plain_data,(void*)plain_data,plain_length);
-    for(int i=plain_length;i<all_plain_data_length;++i) {
+    memcpy((void*)all_plain_data,(void*)plain_data,plain_data_length);
+    for(unsigned int i=plain_data_length;i<all_plain_data_length;++i) {
         all_plain_data[i] = padding;
+    }
+    
+    for(unsigned int i=all_plain_data_length;i<all_plain_data_length+16;++i) {
+        all_plain_data[i] = 0;
     }
     
     unsigned char data_block[16];
     unsigned char out[16];
-    unsigned char* encrypted_data = (unsigned char*)malloc(all_plain_data_length);
-    for(int i=0;i<all_plain_data_length;i+=16) {
+    unsigned char* encrypted_data = (unsigned char*)malloc(all_plain_data_length+1);
+    for(unsigned int i=0;i<all_plain_data_length+16;i+=16) {
         memcpy((void*)(&data_block),(void*)(all_plain_data+i),16);
         AES_cbc_encrypt(data_block,out,16,&aes_key,iv,AES_ENCRYPT);
         memcpy((void*)(encrypted_data+i),(void*)(&out),16);
     }
-
+    
     free(all_plain_data);
-    return encrypted_data;
+    encryptedData* return_struct = (encryptedData*)malloc(sizeof(encryptedData));
+    return_struct->length = all_plain_data_length + 16;
+    return_struct->encrypted_data = encrypted_data;
+    return return_struct;
+}
+
+unsigned char* AES256CBCDecrypt(encryptedData* encrypted_data, unsigned char* key, unsigned char* iv) {
+    AES_KEY aes_key;
+    AES_set_decrypt_key((const unsigned char*)key,256,&aes_key);
+    
+    unsigned char data_block[16];
+    unsigned char out[16];
+    unsigned char* plain_data_all = (unsigned char*)malloc(encrypted_data->length);
+    for(unsigned int i=0;i<encrypted_data->length;i+=16) {
+        memcpy((void*)(&data_block),(void*)(encrypted_data->encrypted_data+i),16);
+        AES_cbc_encrypt(data_block,out,16,&aes_key,iv,AES_DECRYPT);
+        memcpy((void*)(plain_data_all+i),(void*)(&out),16);
+    }
+    
+    for(unsigned int i=encrypted_data->length-16;i<encrypted_data->length;++i) {
+        if(*(plain_data_all+i) != 0) {
+            return NULL;
+        }
+    }
+    
+    unsigned char padding = *(plain_data_all + encrypted_data->length - 17);
+    unsigned int plain_data_length = encrypted_data->length - padding - 16;
+    unsigned char* plain_data = (unsigned char*)malloc(plain_data_length);
+    memcpy((void*)plain_data,(void*)plain_data_all,plain_data_length);
+    
+    free(plain_data_all);
+    return plain_data;
 }
 
 #endif
--- test.cpp
+++ test.cpp
@@ -6,16 +6,21 @@ using namespace std;
 
 int main() {
     string str1 = "test1";
-    string str2 = "test2";
-    unsigned char* result = doubleSHA256(str1.c_str());
-    for(int i=0;i<32;++i) {
-        printf("%02x",result[i]);
+    string str2 = "0000000000000000111111111111111122222222222222223333333333333333";
+    unsigned char* iv = (unsigned char*)malloc(16);
+    memset(iv,0,16);
+    unsigned char* resultmd5 = doubleMD5((const unsigned char*)str1.c_str(),str1.length());
+    unsigned char* resultsha = doubleSHA256((const unsigned char*)str1.c_str(),str1.length());
+    encryptedData* encrypted = AES256CBCEncrypt((const unsigned char*)str2.c_str(),str2.length(),resultsha,iv);
+    for(int i=0;i<encrypted->length;++i) {
+        printf("%02x",encrypted->encrypted_data[i]);
     }
     printf("\n");
-    unsigned char* encrypted = SHA256EncryptAES128CBC((const unsigned char*)str2.c_str(),result);
-    for(int i=0;i<16;++i) {
-        printf("%02x",encrypted[i]);
+    unsigned char* plain = AES256CBCDecrypt(encrypted,resultsha,iv);
+    if(plain == NULL) {
+        printf("NULL");
+    } else {
+        printf((const char*)plain);
     }
-    printf("\n");
     return 0;
 }
