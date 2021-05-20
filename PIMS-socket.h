ef53f249eadb5eb19082016b63fdb2bf3b4a1d89
littlefish12345 <18819303050@163.com>
2021-05-20 13:28:39 +0800

    windows socket library(didn't test)

---
+++ PIMS-socket.h
@@ -0,0 +1,119 @@
+#ifndef PIMS_SOCKET_H
+
+#define PIMS_SOCKET_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+
+#ifdef (WIN32)
+
+#include<winsock2.h>
+
+typedef struct  {
+	SOCKET socket;
+}PIMS_SOCKET;
+
+WSADATA wsaData;
+
+void socketStartup() {
+	WSAStartup(MAKEWORD(2,2),&wsaData);
+}
+
+void socketCleanup() {
+	WSACleanup();
+}
+
+PIMS_SOCKET* createSocket() {
+	SOCKET socket = socket(PF_INET,SOCKET_STREAM,IPPROTO_TCP);
+	PIMS_SOCKET* pims_socket = (PIMS_SOCKET*)malloc(sizeof(PIMS_SOCKET));
+	pims_socket->socket = socket;
+	return pims_socket;
+}
+
+bool bindSocket(char* ip,int port) {
+	struct sockaddr_in socket_address;
+	memset((void*)(&socket_address),0,sizeof(socket_address));
+	socket_address.sin_family = PF_INET;
+	socket_address.sin_addr.s_addr = inet_addr(ip);
+	socket_address.sin_port = htons(port);
+	if(bind(socket,(SOCKETADDR*)socket_address,sizeof(SOCKADDR)) == SOCKET_ERROR) {
+		return false;
+	}
+	listen(socket,5);
+	
+	return true;
+}
+
+bool connectSocket(char* ip,int port) {
+	struct sockaddr_in socket_address;
+	memset((void*)(&socket_address),0,sizeof(socket_address));
+	socket_address.sin_family = PF_INET;
+	socket_address.sin_addr.s_addr = inet_addr(ip);
+	socket_address.sin_port = htons(port);
+	if(connect(socket,(SOCKETADDR*)socket_address,sizeof(SOCKADDR)) == SOCKET_ERROR) {
+		return false;
+	}
+	return true;
+}
+
+PIMS_SOCKET* acceptConnection(PIMS_SOCKET* socket) {
+	SOCKADDR client_address;
+	SOCKET client_socket = accept(socket->socket,&client_address,sizeof(SOCKADDR));
+	PIMS_SOCKET* connection = (PINS_SOCKET*)malloc(sizeof(PIMS_SOCKET));
+	connection->socket = client_socket
+	return connection;
+}
+
+void sendData(PIMS_SOCKET* socket, unsigned char* data, unsigned int length) {
+	send(socket->socket,data,length,0);
+}
+
+void closeSocket(PIMS_SOCKET* socket) {
+	closesocket(socket->socket);
+	free(socket);
+}
+
+unsigned char* receiveData(PIMS_SOCKET* socket,int* status) {
+	unsigned char recv_buffer[256];
+	unsigned int ret = 0;
+	
+	*status = 0;
+	ret = recv(socket->socket,recv_buffer,256,0);
+	if(ret==0) {
+		*status = 1;
+		return NULL;
+	} else if(ret<0) {
+		if(errno==EAGAIN || errno==EWOULDBLOCK || errno==EINTR) {
+			return NULL;
+		} else {
+			*status = -1;
+			return NULL;
+		}
+	}
+
+	unsigned char* recv_data = (unsigned char*)malloc(ret);
+	memcpy((void*)recv_data,(void*)(&recv_buffer),ret);
+	int all_length = ret;
+	unsigned char* new_recv_data;
+	while(ret==256) {
+		ret = recv(socket->socket,recv_buffer,256,0);
+		if(ret<0) {
+			if(errno==EAGAIN || errno==EWOULDBLOCK || errno==EINTR) {
+				break;
+			}
+		} else {
+			*status = -1;
+			return NULL;
+		}
+		
+		all_length += ret;
+		new_recv_data = (unsigned char*)realloc(recv_data,all_length);
+		free(recv_data);
+		recv_data = new_recv_data;
+	}
+
+	return recv_data;
+}
+
+#endif
--- test.cpp
+++ test.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main() {
     string str1 = "test1";
-    string str2 = "0000000000000000111111111111111122222222222222223333333333333333";
-    unsigned char* iv = (unsigned char*)malloc(16);
-    memset(iv,0,16);
+    string str2 = "test2";
+    string str3 = "wdnmd";
     unsigned char* resultmd5 = doubleMD5((const unsigned char*)str1.c_str(),str1.length());
-    unsigned char* resultsha = doubleSHA256((const unsigned char*)str1.c_str(),str1.length());
-    encryptedData* encrypted = AES256CBCEncrypt((const unsigned char*)str2.c_str(),str2.length(),resultsha,iv);
+    unsigned char* resultsha1 = doubleSHA256((const unsigned char*)str1.c_str(),str1.length());
+    unsigned char* resultsha2 = doubleSHA256((const unsigned char*)str2.c_str(),str2.length());
+    encryptedData* encrypted = AES256CBCEncrypt((const unsigned char*)str3.c_str(),str2.length(),resultsha1,resultmd5);
     for(int i=0;i<encrypted->length;++i) {
         printf("%02x",encrypted->encrypted_data[i]);
     }
     printf("\n");
-    unsigned char* plain = AES256CBCDecrypt(encrypted,resultsha,iv);
+    unsigned char* plain = AES256CBCDecrypt(encrypted,resultsha1,resultmd5);
     if(plain == NULL) {
-        printf("NULL");
+        printf("NULL\n");
     } else {
-        printf((const char*)plain);
+        printf("%s\n",plain);
     }
     return 0;
 }
